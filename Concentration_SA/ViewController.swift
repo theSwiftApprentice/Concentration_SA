bda2a2ea7e0fb8075ff92e86a7fee83224707608
theSwiftApprentice <theSwiftApprentice@icloud.com>
2018-01-29 12:00:25 +0100

    Initial Commit

---
+++ Concentration_SA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration_SA
+//
+//  Created by Marc Gidaszewski on 29.01.18.
+//  Copyright © 2018 The Swift Apprentice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
